086e0ef6b19111ea404a5ef96058396c2be5fd89
Dhiraj-JC <dhiraj@trainitsolutions.com>
2022-06-07 16:57:19 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [users, setUsers] = useState([]);
+  let [name, setName] = useState('');
+  let [age, setAge] = useState('');
+  let [id, setId] = useState('');
+  let [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/users`)
+      .then((response) => response.json())
+      .then((users) => setUsers(users));
+  },[]);
+
+  const saveUser = () => {
+    fetch(`http://localhost:3001/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        age
+      })
+    })
+      .then((response) => response.json())
+      .then((users) => {
+        setUsers(users);
+        setName('');
+        setAge('');
+      });
+  }
+
+  const deleteUser = (userId) => {
+    fetch(`http://localhost:3001/users/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((users) => {
+        setUsers(users);
+        setName('');
+        setAge('');
+      });
+  }
+
+  const saveUpdatedUser = () => {
+    fetch(`http://localhost:3001/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        age
+      })
+    })
+      .then((response) => response.json())
+      .then((users) => {
+        setUsers(users);
+        setName('');
+        setAge('');
+        setId('');
+        setEditMode(false);
+      });
+  }
+
+  const editUser = (user) => {
+    setName(user.name);
+    setAge(user.age);
+    setId(user.id);
+    setEditMode(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{editMode ? 'Edit ' : 'Add '} User</h1>
+      <div>
+        <label>Name:</label>
+      <input type="text" value={name} onChange={(event)=> {setName(event.target.value)}}/>
+      </div>
+      <div>
+      <label>Age:</label>
+      <input type="number" value={age} onChange={(event)=> {setAge(event.target.value)}}/>
+      </div>
+      {editMode ? <button onClick={saveUpdatedUser}>update</button> : <button onClick={saveUser}>Save</button>}
+      
+      <br/>
+      <br/>
+      <h1>Users</h1>
+      <table border='1'>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Operation</th>
+        </tr>
+        </thead>
+        <tbody>
+        {users.map((user,index) => (
+          <tr key={index}>
+            <td>{user.name}</td>
+            <td>{user.age}</td>
+            <td><button onClick={(e)=> editUser(user)}>Edit</button> <button onClick={(e)=> deleteUser(user.id)}>Delete</button></td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
